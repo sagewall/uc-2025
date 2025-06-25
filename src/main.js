1f2aede605f5df32fc290d62f54b9d133e8dd925
Sage Wall <swall@esri.com>
2025-06-25 16:10:32 -0600

    chore: initial commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import esriConfig from "@arcgis/core/config";
+import "@arcgis/map-components/components/arcgis-editor";
+import "@arcgis/map-components/components/arcgis-layer-list";
+import "@arcgis/map-components/components/arcgis-map";
+import "@arcgis/map-components/components/arcgis-zoom";
+import "./style.css";
+
+// Set the API key or portal URL if needed
+//esriConfig.apiKey= "Your-Access-Token";
+esriConfig.portalUrl = "https://jsapi.maps.arcgis.com/";
