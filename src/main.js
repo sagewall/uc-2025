f759f94ed33953b5d464d8181f317fe0642cb8b8
Sage Wall <swall@esri.com>
2025-07-01 10:10:23 -0600

    chore: add apiKeys example

--- src/main.js
+++ src/main.js
@@ -7,4 +7,15 @@ import "./style.css";
 
 // Set the API key or portal URL if needed
 //esriConfig.apiKey= "Your-Access-Token";
+// esriConfig.apiKeys = {
+//   scopes: [
+//     {
+//       // The API key value.
+//       token: "YOUR_ACCESS_TOKEN",
+//       // An array of URLs that the API key applies to.
+//       urls: ["https://basemaps.arcgis.com", "https://services3.arcgis.com"],
+//     },
+//     // More scopes can be added here...
+//   ],
+// };
 esriConfig.portalUrl = "https://jsapi.maps.arcgis.com/";
